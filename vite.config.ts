f99a497f032240af91610bf7ec15fec8b4f95959
SoyLaw <lautaroelosegui194@gmail.com>
2025-09-23 12:43:36 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ScanEat/', // 👈 ¡ESTO es lo nuevo!
+    base: '/ScanToEat/', // 👈 ¡ESTO es lo nuevo!
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
